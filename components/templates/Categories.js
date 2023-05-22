bd8647fafe2d33325c1f8a288eeac07529131753
Ali Yousefi <alifordevelpment@gmail.com>
2023-05-22 21:52:07 +0430

    make category page

--- components/templates/Categories.js
+++ components/templates/Categories.js
@@ -1,23 +1,59 @@
+import { useEffect, useState } from "react";
 import styles from "./Categories.module.css";
+import { useRouter } from "next/router";
+import Card from "../modules/cards/Card";
 
-const Categories = () => {
+const Categories = ({ data }) => {
+  const router = useRouter();
+
+  const [query, setQuery] = useState({ difficulty: "", time: "" });
+  useEffect(() => {
+    const { difficulty, time } = router.query;
+    if (query.difficulty !== difficulty || query.time !== time) {
+      setQuery({ difficulty, time });
+    }
+  }, []);
+
+  const changeHandler = (event) => {
+    setQuery({ ...query, [event.target.name]: event.target.value });
+  };
+
+  const searchHandler = () => {
+    router.push({
+      pathname: "/categories",
+      query,
+    });
+  };
+
   return (
     <div className={styles.container}>
       <h2>Categories</h2>
       <div className={styles.subContainer}>
         <div className={styles.select}>
-          <select>
-            <option>Difficulty</option>
-            <option>Easy</option>
-            <option>Medium</option>
-            <option>Hard</option>
+          <select
+            value={query.difficulty}
+            name="difficulty"
+            onChange={changeHandler}
+          >
+            <option value="">Difficulty</option>
+            <option value="Easy">Easy</option>
+            <option value="Medium">Medium</option>
+            <option value="Hard">Hard</option>
           </select>
-          <select>
-            <option>Coocking Time</option>
-            <option>More than 30 min</option>
-            <option>Less than 30 min</option>
+          <select value={query.time} onChange={changeHandler} name="time">
+            <option value="">Coocking Time</option>
+            <option value="more">More than 30 min</option>
+            <option value="less">Less than 30 min</option>
           </select>
-          <button>Search</button>
+          <button onClick={searchHandler}>Search</button>
+        </div>
+        <div className={styles.cards}>
+          {!data.length ? (
+            <img src="/images/search.png" alt="category" />
+          ) : null}
+          {data.map((food) => (
+            <Card data={food} key={food.id} />
+          ))}
         </div>
       </div>
     </div>
--- pages/categories/index.js
+++ pages/categories/index.js
@@ -1,8 +1,46 @@
 import Categories from "@/components/templates/Categories";
 import React from "react";
 
-const index = () => {
-  return <Categories />;
+const index = ({ data }) => {
+  console.log(data);
+  return <Categories data={data} />;
 };
 
 export default index;
+export async function getServerSideProps(context) {
+  const {
+    query: { difficulty, time },
+  } = context;
+
+  const res = await fetch("http://localhost:3001/data");
+  const data = await res.json();
+
+  const filteredData = data.filter((item) => {
+    const difficultyResult = item.details.filter(
+      (detail) => detail.Difficulty && detail.Difficulty === difficulty
+    );
+
+    const timeResult = item.details.filter((detail) => {
+      const cookingTime = detail["Cooking Time"] || "";
+      const [timeDetail] = cookingTime.split(" ");
+
+      if (time === "less" && timeDetail && +timeDetail <= 30) {
+        return detail;
+      } else if (time === "more" && +timeDetail > 30) {
+        return detail;
+      }
+    });
+
+    if (time && difficulty && timeResult.length && difficultyResult.length) {
+      return item;
+    } else if (!time && difficulty && difficultyResult.length) {
+      return item;
+    } else if (time && !difficulty && timeResult.length) {
+      return item;
+    }
+  });
+
+  return {
+    props: { data: filteredData },
+  };
+}
