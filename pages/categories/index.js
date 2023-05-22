c02efd7be20e362e54d1cdafa7abbd3dcd0ed504
Ali Yousefi <alifordevelpment@gmail.com>
2023-05-22 19:14:27 +0430

    make menu page and detail page od foods

---
+++ pages/categories/index.js
@@ -0,0 +1,8 @@
+import Categories from "@/components/templates/Categories";
+import React from "react";
+
+const index = () => {
+  return <Categories />;
+};
+
+export default index;
---
+++ pages/menu/[menuId].js
@@ -0,0 +1,49 @@
+import DetailePage from "@/components/templates/DetailePage";
+import { useRouter } from "next/router";
+import React from "react";
+
+const details = ({ data }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h2>Loading page....</h2>;
+  }
+  return (
+    <div>
+      <DetailePage {...data} />
+    </div>
+  );
+};
+
+export default details;
+
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:3001/data");
+  const json = await res.json();
+  const data = json.slice(0, 10);
+
+  const paths = data.map((food) => ({
+    params: { menuId: food.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const res = await fetch(`http://localhost:3001/data/${params.menuId}`);
+  const data = await res.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: { data },
+    revalidate: 10,
+  };
+}
